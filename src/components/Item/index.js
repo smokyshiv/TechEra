7b5da21d2d5db02a1bce3bdd6c8ca9f17fb86974
smokyshiv <chnarsappa6@mail.com>
2023-12-26 05:00:04 +0000

    first

---
+++ src/components/Item/index.js
@@ -0,0 +1,42 @@
+import {Link} from 'react-router-dom'
+
+import {Ci, Cn, Li} from './style'
+
+import './index.css'
+
+const Item = props => {
+  const {details} = props
+  const {id, name, logoUrl} = details
+  return (
+    <Li>
+      <Link to={`/courses/${id}`} className="link-el">
+        <Ci src={logoUrl} alt={name} />
+        <Cn>{name}</Cn>
+      </Link>
+    </Li>
+  )
+}
+
+export default Item
+
+/*
+import {Link} from 'react-router-dom'
+
+import './index.css'
+
+const Item = props => {
+  const {details} = props
+  const {id, name, logoUrl} = details
+  return (
+    <li>
+      <Link to={`/courses/${id}`} className="link-el">
+        <img src={logoUrl} alt={name} />
+        <p>{name}</p>
+      </Link>
+    </li>
+  )
+}
+
+export default Item
+
+*/
